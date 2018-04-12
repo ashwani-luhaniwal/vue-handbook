0b6ce82a2bfcc3d1c406a7cd143e205e44c8d11c
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2018-04-12 08:26:22 -0700

    Logic to handle recaptcha in vue

---
+++ Examples/Vue-Recaptcha/index.js
@@ -0,0 +1,54 @@
+new Vue({
+    el: '#app',
+    components: {
+        'vue-recaptcha': VueRecaptcha
+    },
+    methods: {
+        submit: function () {
+            // this.submit = "submitting"
+            this.$refs.recaptcha.execute();
+        },
+        onCaptchaVerified: function (recaptchaToken) {
+            const self = this;
+            self.status = "submitting";
+            self.$refs.recaptcha.reset();
+            axios.post("https://vue-recaptcha-demo.herokuapp.com/signup", {
+                email: self.email,
+                password: self.password,
+                recaptchaToken: recaptchaToken
+            }).then(response => {
+                self.successfulServerResponse = response.data.message;
+            }).catch(err => {
+                self.serverError = getErrorMessage(err);
+
+                // helper to get a displayable message to the user
+                function getErrorMessage(err) {
+                    let responseBody;
+                    responseBody = err.response;
+                    if (!responseBody) {
+                        responseBody = err;
+                    }
+                    else {
+                        responseBody = err.response.data || responseBody;
+                    }
+                    return responseBody.message || JSON.stringify(responseBody);
+                }
+            }).then(() => {
+                self.status = "";
+            });
+        },
+        onCaptchaExpired: function () {
+            this.$refs.recaptcha.reset();
+        }
+    },
+    data() {
+        return {
+            email: "",
+            password: "",
+            passwordConfirmation: "",
+            status: "",
+            successfulServerResponse: "",
+            serverError: ""
+        };
+    }
+});
